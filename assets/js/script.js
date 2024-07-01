52cb198cc66bac29c1984b5177639e25f8fb4693
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-07-01 17:23:52 +0000

    create main function for random numbers and add event-listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
+
+document.addEventListener ("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    
+    for(let button of buttons) {
+            button.addEventListener("click", function() {
+                if (this.getAttribute("data-type") === "submit") {
+                    alert("You clicked Submit!");
+                } else {
+                    let gameType = this.getAttribute("data-type");
+                    alert(`you clicked ${gameType}!`);
+                }
+     })
+    }
+})
+
 function runGame() {
 
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    console.log(num2)
+    console.log(num1)
+    console.log(num1 + num2)
 }
+
+runGame();
 
 function checkAnswer() {
 
